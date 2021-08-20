c85903b29723e8fbf5f69915d2cd6951dae8562e
SpaceflightCoder <88935957+SpaceflightCoder@users.noreply.github.com>
2021-08-20 17:48:07 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const aoijs = require("aoi.js")
+
+const bot = new aoijs.Bot({
+token: "TOKEN", //Discord Bot Token
+prefix: "PREFIX" //Discord Bot Prefix
+})
+bot.onMessage() //Allows to execute Commands
+
+bot.command({
+name: "ping", //Trigger name (command name)
+code: `Pong! $pingms` //Code
+})
+
+bot.readyCommand({
+    channel: "", //You can use this or not
+    code: `$log[Ready on $userTag[$clientID]]` //Example Ready on Client
+})
